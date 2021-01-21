fbbc0417b834e1b9b2b533fd2de7e76b2a3e1062
Azizullo <63806547+ahmymas@users.noreply.github.com>
2021-01-21 18:03:00 +0500

    Update Fizzbuzz.go

--- Fizzbuzz.go
+++ Fizzbuzz.go
@@ -4,15 +4,14 @@ import "fmt"
 
 func main() {
     for i := 1; i <= 100; i++ {
-        if i%3 == 0 {
-          fmt.Println("fizz")
-        }
-        if i%5 == 0 {
-          fmt.Println("buzz")
-        }
-        if i%3 != 0 && i%5 != 0 {
-           fmt.Println("%d", i)
-        }
-        fmt.Println("\n")
+	if i % 3 == 0 && i % 5 == 0 {
+		fmt.Println("fizzbuzz")
+	} else if i % 3 == 0 {
+		fmt.Println("fizz")
+	} else if i % 5 == 0 {
+		fmt.Println("buzz")
+	} else {
+		fmt.Println(i)
+	}
     }
 }
